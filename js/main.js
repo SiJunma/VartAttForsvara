90b61cd585e5a39ef6de1ee86c684b05cdde7fb6
Alina Onishchenko <30931781+SiJunma@users.noreply.github.com>
2021-04-27 19:45:22 +0300

    Add files via upload
    Перекомпоновка и переразмеривание всего

--- js/main.js
+++ js/main.js
@@ -9,3 +9,12 @@ window.onscroll = function () {
         myNav.classList.remove("nav-scrolling");
     }
 };
+
+const filterToggle = document.getElementById('filterBtn');
+const categoryList = document.getElementById('categoryList');
+
+filterToggle.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    filterToggle.classList.toggle('filter__spoiler-btn_open');
+    categoryList.classList.toggle('category-list_closed');
+})
